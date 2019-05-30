60d839f991e9647887128500bfde5f9cc582ca31
Morishita1 <iakwpi@naver.com>
2019-05-30 16:39:23 +0900

    first

---
+++ src/Hello_01.java
@@ -0,0 +1,23 @@
+
+public class Hello_01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("대한민국만세");
+		System.out.println("00000000000");
+		System.out.println("OOOOOOOOOOO");
+		System.out.println("ooooooooooo");
+		System.out.println("IIIIIIIIIIII");
+		System.out.println("iiiiiiiiiiii");
+		System.out.println("llllllllllll");
+		System.out.println("LLLLLLLLLLLL");
+		System.out.println("111111111111");
+		
+		boolean bYes = 10 != 20;
+		bYes = 10 >= 20;
+		bYes = 10 <= 20;
+		bYes = 10 == 20;
+		
+	}
+
+}
